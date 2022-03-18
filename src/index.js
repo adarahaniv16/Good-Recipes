6d8580fb53793d66b59cb51226efc8dcb348bc04
Diandara Tresya Haniva <diandara.haniva19071@student.unsika.ac.id>
2022-03-18 14:48:21 +0700

    installasi popperjs/core
    installasi popperjs/core melalui npm

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
+import "regenerator-runtime";
+import "@popperjs/core";
 import "./scripts/components/nav-bar.js";
 import "./scripts/components/search-bar.js";
-import "regenerator-runtime";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 import "./style/main.css";
